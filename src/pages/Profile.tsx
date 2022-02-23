e2d42678d576d16b5bc27a5d00bb7fefb241423e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-02-23 11:14:40 +0900

    update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { getAuth, User } from 'firebase/auth'
+import React, { useState, useEffect, ChangeEvent } from 'react'
+import { getAuth, updateProfile, User } from 'firebase/auth'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { useNavigate, Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const Profile = () => {
   const [user, setUser] = useState<User | null>(null)
 
   const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
 
-  useEffect(() => {
-    setUser(auth.currentUser!)
-  }, [auth.currentUser])
+  const [formData, setFormData] = useState({
+    name: auth.currentUser?.displayName,
+    email: auth.currentUser?.email,
+  })
+
+  const { name, email } = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser?.displayName !== name) {
+        await updateProfile(auth.currentUser!, {
+          displayName: name,
+        })
+
+        const userRef = doc(db, 'users', auth.currentUser!.uid)
+
+        await updateDoc(userRef, {
+          name,
+        })
+      }
+    } catch (error) {
+      console.info(error)
+      toast.error('Coul not update profile details')
+    }
+  }
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
 
   return user ? <h1>{user.displayName}</h1> : <h1> Not logged In</h1>
 }
